9ec4c6157fd740be4149a010772ab01df7ed5649
FeFe02 <63205758+FeFe02@users.noreply.github.com>
2024-03-31 19:42:59 -0500

    Add files via upload

---
+++ src/Queue.java
@@ -0,0 +1,50 @@
+public class Queue<T> {
+    private Node<T> front;
+    private Node<T> rear;
+    private int size;
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Queue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+
+    public void enqueue(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (empty()) {
+            front = newNode;
+        } else {
+            rear.next = newNode;
+        }
+        rear = newNode;
+        size++;
+    }
+
+    public T dequeue() {
+        if (empty()) {
+
+            return null;
+        }
+        T data = front.data;
+        front = front.next;
+        if (front == null) {
+            rear = null;
+        }
+        size--;
+        return data;
+    }
+
+    public boolean empty() {
+        return size == 0;
+    }
+}
