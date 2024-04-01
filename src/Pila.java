4dea58959e10aa6260d1989d379927de3d13f121
FeFe02 <63205758+FeFe02@users.noreply.github.com>
2024-03-31 20:20:59 -0500

    Update Pila.java

--- src/Pila.java
+++ src/Pila.java
@@ -1,69 +1,90 @@
-public class Pila {
-
-    private class pilaNodo{
+public class Stack {
+    
+    //Creación del nodo
+    private class stackNodo{
         Nodo data;
-        pilaNodo next;
-        pilaNodo (Nodo x){
-            data = x;
-            next = null;
+        stackNodo next;
+        
+        public stackNodo (T val, Nodo x){
+            data = val;
+            next = nodo;
         }
+
+        public void setData (T val) {
+	        data = val;
+	    }
+	
+	    public T getData () {
+	        return data;
+	    }
+	
+    	public Node getNext () {
+    	    return next;
+    	}
+    	
+    	public void setNext (Node n) {
+    	    next = n;
+    	}
     }
 
-        pilaNodo top;
-        int size;
-        Pila(){
-            this.top = null;
-            this.size = 0;
-        }
 
-    public void pilaPush(Nodo x){
-        pilaNodo element = new pilaNodo(x);
-        element.next = top;
-        top = element;
+    //Creación del stack a usar
+    stackNodo top;
+    int size;
+
+    //Constructor de la clase
+    public Stack(){
+        top = null;
+        size = 0;
+    }
+
+    //Método Push
+    public void stackPush(T val){
+        stackNodo nodo = new stackNodo(val, top);
+        top = nodo;
         System.out.println("Pusheo correcto");
         size++;
     }
 
-    public Nodo pilaPop(){
+    //Método Pop
+    public T stackPop(){
         if (top == null) return null;
-        Nodo topData = top.data;
-        pilaNodo temp = top;
-        top = top.next;
-        return topData;
+        else{
+            data = top.getData();
+            stackNodo temp = top;
+            top = top.getNext();
+            tmp.setNext(null);
+            size--;
+        }
+        return data;
     }
 
-    public int pilaSize(){
+    //Método size
+    public int stackSize(){
         return size;
     }
 
-    public boolean pilaVacia(){
+    //Método IsEmpty
+    public boolean stackVacio(){
         return top == null;
+    }
+
+    //Método Peek
+    public T peek(){
+        T data = null
+        if (top == null) return null;
+        else data = top.getData();
+        return data;
     }
 
 
 
 }
 
-class Nodo {
-    String nombre;
-    String facultad;
-    String universidad;
-    String restaurante;
-    Nodo next;
-  
-    public Nodo(String nombre, String facultad, String universidad, String restaurante) {
-      this.nombre = nombre;
-      this.facultad = facultad;
-      this.universidad = universidad;
-      this.restaurante = restaurante;
-      this.next = null;
-    }
-  }
-
 class Main{
     public static void main (String args[]){
-        Pila s = new Pila();
-        s.pilaPush("Ana", "Ingeniería", "Universidad Nacional", "Yutakeuchi");
+        Stack s = new Stack();
+        s.stackPush("Ana", "Ingeniería", "Universidad Nacional", "Yutakeuchi");
         //s.pilaPush(20);
         System.out.println(s.pilaSize());
 
